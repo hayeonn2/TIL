9d86c31abe74155e83c7d369ac2be3440ee00db1
hayoenn2 <dwd1205@naver.com>
2023-02-19 16:47:24 +0900

    add | 구조분해할당으로 props 받기

--- src/Hello.js
+++ src/Hello.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-function Hello(props) {
-  return <div style={{ color: props.color }}>안녕하세요! {props.name}</div>;
+function Hello({ color, name }) {
+  return <div style={{ color }}>안녕하세요! {name}</div>;
 }
 
 export default Hello;
