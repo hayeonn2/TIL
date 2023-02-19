a9dad1905c0e6330afe24359e46c6de0a8e72c19
hayoenn2 <dwd1205@naver.com>
2023-02-19 16:19:47 +0900

    add | props

--- src/App.js
+++ src/App.js
@@ -12,11 +12,7 @@ function App() {
   };
   return (
     <>
-      {/* 주석은 안보여! */}
-      /* 중괄호로 안감싸면 보인다! */ // 이건? 이것도 보여!
-      <Hello
-      // 근데 여기서는 사용할 수 있지롱!
-      />
+      <Hello name="react" />
       <div style={style}>{name}</div>
       <div className="gray-box"></div>
     </>
--- src/Hello.js
+++ src/Hello.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-function Hello() {
-  return <div>안녕하세요!</div>;
+function Hello(props) {
+  return <div>안녕하세요! {props.name}</div>;
 }
 
 export default Hello;
