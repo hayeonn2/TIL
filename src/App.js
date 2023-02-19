320abf692c3eca8ba7616dbf10b816857d484ecf
hayoenn2 <dwd1205@naver.com>
2023-02-19 15:37:17 +0900

    add | style과 className

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import Hello from "./Hello";
 
 function App() {
   const name = "hayeon";
-
+  const style = {
+    backgroundColor: "skyblue",
+    color: "navy",
+    fontSize: "24",
+    padding: "1rem",
+  };
   return (
     <>
       <Hello />
-      <div>{name}</div>
+      <div style={style}>{name}</div>
     </>
   );
 }
