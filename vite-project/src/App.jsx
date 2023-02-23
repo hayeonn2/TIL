55383bf9176522ba348d90c46ad084f8358a2478
hayoenn2 <dwd1205@naver.com>
2023-02-24 02:44:53 +0900

    add | App.jsx 모듈 css 적용시키기

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,18 @@
-import "./App.css";
+import classes from "./App.module.css";
 import { Button } from "./components";
 
 function App() {
+  const handleClick = (e) => {
+    console.log(e.target);
+  };
+
   return (
-    <div className="App">
-      <h2>Button 컴포넌트(stateless)</h2>
-      <Button>Primary Button</Button>
-      <Button>Secondary Button</Button>
+    <div className={classes.container}>
+      <h2 className={classes.headline}>Button 컴포넌트(stateless)</h2>
+      <div role="group" className={classes.buttonGroup}>
+        <Button onClick={handleClick}>Primary Button</Button>
+        <Button onClick={handleClick}>Secondary Button</Button>
+      </div>
     </div>
   );
 }
