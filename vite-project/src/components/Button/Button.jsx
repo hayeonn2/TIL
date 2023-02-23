32ff4874c90c2103ca209eb5336ac2d63564fac9
hayoenn2 <dwd1205@naver.com>
2023-02-24 02:26:07 +0900

    add | 버튼 컴포넌트 버튼에 style 입히기

--- vite-project/src/components/Button/Button.jsx
+++ vite-project/src/components/Button/Button.jsx
@@ -1,7 +1,19 @@
 import classes from "./Button.module.css";
+import { tokens } from "@/theme/tokens";
 
+const { colors } = tokens;
 export function Button({ mode, ...restProps }) {
-  return <button type="button" {...restProps} />;
+  return (
+    <button
+      type="button"
+      className={classes.component}
+      style={{
+        backgroundColor: colors.primary["500"],
+        color: colors.white,
+      }}
+      {...restProps}
+    />
+  );
 }
 
 Button.defaultProps = {
